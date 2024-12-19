3f05f9154b197a2a6827be3e4cd4d0c5006a9577
iamzainn <iamzainnf@gmail.com>
2024-12-19 22:23:30 +0500

    update medicine

--- src/lib/validations/medicine-order.ts
+++ src/lib/validations/medicine-order.ts
@@ -1,16 +1,35 @@
-// lib/validations/medicine-order.ts
-import * as z from "zod"
+import { z } from "zod"
+
+export const medicineSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  price: z.number(),
+})
+
+// Pharmacy type for dropdown
+export const pharmacySchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  city: z.string(),
+  metadata: z.object({
+    description: z.string(),
+    location: z.string(),
+    services: z.array(z.string()),
+  }).optional(),
+})
 
 
 
 export const medicineOrderSchema = z.object({
-  medicines: z.string().optional(),
-  prescriptionUrl: z.string().optional(), // Changed from File to string URL
+  medicines: z.array(z.string()).default([]),
+  prescriptionUrl: z.string().optional(),
   address: z.string().min(1, "Address is required"),
+  email: z.string().email("Invalid email address"),
   phoneNumber: z.string().regex(/^\d{11}$/, "Phone number must be 11 digits"),
   patientName: z.string().min(1, "Patient name is required"),
   pharmacyName: z.string().optional(),
   paymentMethod: z.enum(["CASH_ON_DELIVERY", "CARD"]),
+  totalAmount: z.number().optional(),
 })
 
 export type MedicineOrderForm = z.infer<typeof medicineOrderSchema>
