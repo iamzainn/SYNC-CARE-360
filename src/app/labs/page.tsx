785ad4686de4db7f4c6fd1a4da3d8f28d8e291ea
iamzainn <iamzainnf@gmail.com>
2024-12-21 18:44:07 +0500

    fix

---
+++ src/app/labs/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+const LabPage = () => {
+
+    redirect("/labs/chughtai-lhr")
+  return (
+     <>
+     
+     
+     </> 
+  )
+}
+
+export default LabPage
